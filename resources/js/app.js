4fa79ef41aeb6d02c6a27f7371d6264e83c18d8e
imdhemy <imdhemy@gmail.com>
2020-09-24 02:10:00 +0200

    Install react native web

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import { Provider } from 'react-redux'
 import { combineReducers, createStore } from 'redux'
 import reducers from './config/reducers'
 import Example from './screens/Example'
+import { View } from 'react-native-web'
 
 const React = require('react')
 const ReactDOM = require('react-dom')
@@ -12,9 +13,9 @@ const store = createStore(combineReducers(reducers))
 class App extends React.Component {
     render () {
         return (
-            <div>
+            <View>
                 <Example/>
-            </div>
+            </View>
         )
     }
 }
